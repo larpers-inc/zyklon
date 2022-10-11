1f25f9633f73d547f04a63db6b49896d490c4731
V1li <59320967+V1li@users.noreply.github.com>
2022-10-11 17:17:51 +0300

    Rgb text

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Dont open the jar file u clown, put it in your mods folder.");
-        System.exit(0);
-    }
-}
